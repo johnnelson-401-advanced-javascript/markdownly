7c12ab68c232ddfeb47de957ceb396e26ddbe704
Sam Jespersen <heyheysamj@gmail.com>
2019-11-13 11:40:20 -0800

    initial redux scaffolding + Document.js

--- src/components/markdown/Document.js
+++ src/components/markdown/Document.js
@@ -2,25 +2,39 @@ import React, { PureComponent } from 'react';
 import Preview from './Preview';
 import Editor from './Editor';
 import styles from './Document.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Document extends PureComponent {
-  state = {
-    markdown: '# Hi there'
-  };
 
-  updateMarkdown = ({ target }) => {
-    this.setState({ markdown: target.value });
-  };
 
-  render() {
-    const { markdown } = this.state;
-    return (
-      <>
-        <div className={styles.Document}>
-          <Editor markdown={markdown} updateMarkdown={this.updateMarkdown} />
-          <Preview markdown={markdown} />
-        </div>
-      </>
-    );
+const Document = ({ markdown, updateMarkdown }) => {
+
+  return (
+    <>
+      <div className={styles.Document}>
+        <Editor markdown={markdown} updateMarkdown={updateMarkdown} />
+        <Preview markdown={markdown} />
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = state => ({
+  markdown: getMarkdown(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateMarkdown({ target }) {
+    dispatch({ type: UPDATE_MARKDOWN, payload: target.value });
   }
-}
+});
+
+Document.propTypes = {
+  markdown: PropTypes.string.isRequired,
+  updateMarkdown: PropTypes.func.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Document);

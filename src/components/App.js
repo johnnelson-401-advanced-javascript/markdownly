7e3dfecee80bae4b86bca0343bc7e778e0a803eb
Sam Jespersen <heyheysamj@gmail.com>
2019-11-14 17:17:57 -0800

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Document from './markdown/Document';
+import { Tab } from './markdown/Tab';
 import Header from './header/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Document />
-    </>
+      <Route path="/" component={Document} />
+      <Route path="/:id" component={Tab} />
+    </Router>
   );
 }

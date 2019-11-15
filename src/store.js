848d232abcccb35e7231ed9f4da53927aef87711
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-15 13:56:49 -0800

    store.js

--- src/store.js
+++ src/store.js
@@ -1,18 +1,26 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
-
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
 import { saveState } from './utils/localStorage';
 
 
-store.subscribe(() => {
-  saveState(store.getState().editor);
-});
+const middleWare = store => next => action => {
+  
+  store.subscribe(() => {
+    saveState(store.getState().editor);
+  });
+
+next(action);
+
+}
+
+export default createStore(
+  reducer,
+  applyMiddleware(
+    middleWare
+  )
+);
 
 
-export default store;
+
+
+

759f190d618ef8cff20f3e082cea2f5c5c2c8550
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-18 14:06:12 -0800

    removed commented out code

--- src/components/markdown/Document.js
+++ src/components/markdown/Document.js
@@ -3,7 +3,6 @@ import Preview from './Preview';
 import Editor from './Editor';
 import styles from './Document.css';
 import { useSelector, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
 import { TabBar } from './TabBar';
 import { getMarkdown, getFiles, getFocus, getEditTitle } from '../../selectors/markdownSelectors';
 import { updateMarkdown, changeFile, newTab, deleteTab, changeTitle, toggleEdit } from '../../actions/markdownActions';
@@ -18,7 +17,6 @@ export default function Document() {
   
   const dispatch = useDispatch();
   
-
   const changeMarkdown = ({ target }) => dispatch(updateMarkdown(target.value));
   const handleClick = ({ currentTarget }) => dispatch(changeFile(currentTarget.id));
   const handleAdd = () => dispatch(newTab());
@@ -26,21 +24,7 @@ export default function Document() {
   const handleTitle = ({ target }) => dispatch(changeTitle(target.value, target.id));
   const handleTitleEdit = id => dispatch(toggleEdit(id));
   
-  Document.propTypes = {
-    markdown: PropTypes.string.isRequired,
-    changeMarkdown: PropTypes.func.isRequired,
-    files: PropTypes.array.isRequired,
-    handleAdd: PropTypes.func,
-    handleClick: PropTypes.func.isRequired,
-    handleDelete: PropTypes.func.isRequired,
-    focus: PropTypes.string.isRequired,
-    handleTitle: PropTypes.func.isRequired,
-    editTitle: PropTypes.shape({
-      editInput: PropTypes.bool.isRequired,
-      id: PropTypes.string.isRequired
-    }),
-    handleTitleEdit: PropTypes.func.isRequired
-  };
+  
   return (
     <>
       <div className={styles.Document}>
